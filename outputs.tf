784b8b81d858153b2fe86dfc41a58fa0047d7934
ParzivalWins <exercisetech@gmail.com>
2017-02-20 15:31:45 -0800

    Test Commit from Tablet

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "address" {
     value = "Go to `${aws_instance.main.public_ip}` to view the deployed instance. It may take a minute or two for the instance to provision"
 }
+
+#modifying code 
+#git client
